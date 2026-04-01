f43ac4057c0244e52ddac296aaf015284903fa7b
grzracz <grz.raczek@gmail.com>
2026-04-01 14:15:56 +0200

    refactor: remove Neutralino dependencies, delete old service files

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { init } from '@neutralinojs/lib'
-import { setNeutralinoActive } from '@/services/platform'
+import { initPlatform } from '@/services/platform'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import './index.css'
 import '@/store/console'
 import App from './App'
 
 async function bootstrap() {
-  if (window.NL_PORT) {
-    await init()
-    setNeutralinoActive(true)
-  }
+  await initPlatform()
 
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
